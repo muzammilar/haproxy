ae7ce91baa31f1fb63d5b1cb6b9be87a51941444
Remi Tricot-Le Breton <rlebreton@haproxy.com>
2026-05-06 14:16:39 +0200

    MINOR: ssl: Factorize code from "new/set ssl cert" CLI command
    This allows to perform the same kind of operation without the need for
    an appctx.

--- include/haproxy/ssl_ckch.h
+++ include/haproxy/ssl_ckch.h
@@ -48,6 +48,8 @@ struct ckch_store *ckch_store_new(const char *filename);
 void ckch_store_free(struct ckch_store *store);
 void ckch_store_replace(struct ckch_store *old_ckchs, struct ckch_store *new_ckchs);
 int ckch_store_load_files(struct ckch_conf *f, struct ckch_store *c, int cli, const char *file, int linenum, char **err);
+int ckch_store_create(char *path, char **err);
+int ckch_store_load_payload(char *path, char *payload, char **err);
 
 /* ckch_conf functions */
 
